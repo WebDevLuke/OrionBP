4999d136a78fee0c897d15f1a24cb35c32cc791d
Luke Harrison <luke@lukeharrison.net>
2016-02-11 23:57:58 +0000

    added js stuff

--- dev/js/global.js
+++ dev/js/global.js
@@ -30,33 +30,6 @@
 		return false;
 	});
 
-
-/*
-|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-| IF IE 10 or 11 append stylesheet
-|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-*/
-
-// function GetIEVersion() {
-// 	var sAgent = window.navigator.userAgent;
-// 	var Idx = sAgent.indexOf("MSIE");
-
-// 	// If IE, return version number.
-// 	if (Idx > 0)
-// 		return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
-
-// 	// If IE 11 then look for Updated user agent string.
-// 	else if (!!navigator.userAgent.match(/Trident\/7\./))
-// 		return 11;
-// 	else
-// 		return 0; // It is not IE
-// }
-
-
-// if (GetIEVersion() === 10 || GetIEVersion() === 11 ) {
-// 	$('head').append('<link rel="stylesheet" type="text/css" href="css/ie.css">');
-// }
-
 /*
 |--------------------------------------------------------------------
 | GENERATE CURRENT YEAR
@@ -68,3 +41,5 @@ var currentYear = (new Date).getFullYear();
 $(document).ready(function() {
 	$(".year").text( (new Date).getFullYear() );
 });
+
+console.log("hello");
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 /*
 |--------------------------------------------------------------------
@@ -32,11 +35,6 @@ gulp.task('images', function(){
 	.pipe(gulp.dest('dist/img/'))
 });
 
-// Copy Files Wrapper Function
-function gulpCopy(file, destination){
-	gulp.start("copy");
-};
-
 // Copy Misc Files Task
 gulp.task('copy', function() {
 	// Copy specified folders and contents
@@ -52,6 +50,31 @@ gulp.task('copy', function() {
 	.pipe(gulp.dest('dist/'));
 });
 
+// Combine JS
+gulp.task('js-combine', function() {
+	return gulp.src([
+		'./dev/js/vendor/jquery-1.12.0.min.js',
+		'./dev/js/vendor/modernizr.js',
+		'./dev/js/global.js',
+	])
+	.pipe(concat('core.js'))
+	.pipe(gulp.dest('./dist/js'));
+});
+
+// Minify JS
+gulp.task('js-min', ['js-combine'], function() {
+	gulp.src('./dist/js/core.js')
+	.pipe(rename("core.min.js"))
+	.pipe(uglify())
+	.pipe(gulp.dest('./dist/js/'));
+});
+
+// Bring JS tasks together
+gulp.task('js', function() {
+	gulp.start("js-combine");
+	gulp.start("js-min");
+});
+
 /*
 |--------------------------------------------------------------------
 |  GULP PRODUCTION FUNCTIONS
@@ -60,16 +83,22 @@ gulp.task('copy', function() {
 
 // WATCH FUNCTION
 gulp.task("watch", function() {
+	// Images
+	gulp.watch('dev/img/*.+(png|jpg|gif|svg)',['images']);
 	// SASS
 	gulp.watch('dev/sass/**/*.scss',['sass']);
+	// JS
+	gulp.watch('dev/js/**/*.js',['js']);
 });
 
 // BUILD FUNCTION
-gulp.task('default',function() {
-	// Image MIN
+gulp.task('build',function() {
+	// Images
 	gulp.start("images");
-	// Copy File
+	// Copy Files
 	gulp.start("copy");
 	// SASS
 	gulp.start("sass");
+	// JS
+	gulp.start("js");
 });
