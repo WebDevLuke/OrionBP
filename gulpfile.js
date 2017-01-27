7a57fa0f24a7ecdcfb1313d89a1023eebbbee06b
Luke Harrison <luke@lukeharrison.net>
2017-01-27 22:26:35 +0000

    Refined objects

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ var inject = require('gulp-inject');
 
 // If minify is true then css & js will be minified
 // This is in case the code needs to be maintained by a less-technical developer
-var minify = true;
+var minify = false;
 
 /*
 |--------------------------------------------------------------------
